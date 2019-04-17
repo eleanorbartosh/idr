5f4e2ce87499f1dd3c8405a2de47eb2de425b94f
Scott <sstrubberg@us.ibm.com>
2019-04-17 16:23:35 -0500

    homepage starting to come together, building out the nav

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ArrowRight24 } from '@carbon/icons-react';
 import { Link } from 'gatsby';
 
 export class HomepageHeader extends React.Component {
@@ -30,7 +29,7 @@ export class HomepageHeader extends React.Component {
               <div className="ibm--col-lg-4 ibm--offset-lg-4">{homeTitle}</div>
             </div>
           </div>
-          <img src={bannerImg} alt="Carbon Design System banner image" />
+          {/* <img src={bannerImg} alt="Carbon Design System banner image" /> */}
         </section>
         <section className="homepage--callout">
           <div className="ibm--grid">
@@ -38,9 +37,7 @@ export class HomepageHeader extends React.Component {
               <aside
                 aria-label="header callout"
                 className="ibm--col-md-8 ibm--col-lg-8 ibm--offset-lg-4">
-                <h4
-                  className="bx--type-heading-02"
-                  style={{ marginBottom: `2rem` }}>
+                <h4 className="bx--type-heading-02">
                   What is Design
                   <br />
                   Research
@@ -62,33 +59,38 @@ export class HomepageHeader extends React.Component {
   }
 }
 
-export class HomepageFooter extends React.Component {
+export class HomepageTileNav extends React.Component {
   render() {
     return (
-      <div className="homepage--callout homepage--callout--blue">
+      <div className="homepage--callout homepage--callout--pink">
         <div className="ibm--grid">
           <section className="ibm--row">
-            <aside
-              aria-label="footer callout"
-              className="ibm--col-md-4 ibm--col-lg-4 ibm--offset-lg-4">
-              <h3>
-                Wondering how <br />
-                to contribute?
-              </h3>
-            </aside>
-            <div className="ibm--col-lg-8 ibm--col-md-4">
-              <div className="homepage--footer__info">
-                <p className="bx--type-expressive-paragraph-01">
-                  We welcome all feedback, designs, or ideas in order to produce
-                  the best possible experience for our users. If you&rsquo;re
-                  interested in contributing, check out our contributing
-                  guidelines to get started.
-                </p>
-                <Link to="/contributing/governance" alt="Start contributing">
-                  Start contributing
-                  <ArrowRight24 aria-label="Start Contributing" />
-                </Link>
-              </div>
+            <div
+              aria-label="guiding principles"
+              className="ibm--col-md-4 ibm--col-lg-4 ibm--offset-lg-4 home-page-nav">
+              <p>Guiding Principles</p>
+              <p>A foundation for great experiences</p>
+              <Link to="#" alt="Read more">
+                Read more
+              </Link>
+            </div>
+            <div
+              aria-label="research in practice"
+              className="ibm--col-md-4 ibm--col-lg-4 home-page-nav">
+              <p>Research in practice</p>
+              <p>Establish a shared vocabulary</p>
+              <Link to="#" alt="Read more">
+                Read more
+              </Link>
+            </div>
+            <div
+              aria-label="ethics and responsibilities"
+              className="ibm--col-md-4 ibm--col-lg-4 home-page-nav">
+              <p>Ethics & Responsibilities</p>
+              <p>The design research code of conduct</p>
+              <Link to="#" alt="Read more">
+                Read more
+              </Link>
             </div>
           </section>
         </div>
@@ -106,11 +108,7 @@ export class HomepageWhyDoIt extends React.Component {
             <aside
               aria-label="header callout"
               className="ibm--col-md-8 ibm--col-lg-8 ibm--offset-lg-4">
-              <h4
-                className="bx--type-heading-02"
-                style={{ marginBottom: `2rem` }}>
-                Why do it?
-              </h4>
+              <h4 className="bx--type-heading-02">Why do it?</h4>
             </aside>
             <div
               className="ibm--col-lg-8 ibm--col-md-8 ibm--offset-lg-4"
--- src/templates/page.js
+++ src/templates/page.js
@@ -14,9 +14,9 @@ import PageHeader from '../components/PageHeader';
 import NextPrevious from '../components/NextPrevious';
 
 import {
-  HomepageFooter,
   HomepageHeader,
   HomepageWhyDoIt,
+  HomepageTileNav,
 } from '../components/Homepage/Homepage';
 
 export default ({ data }) => {
@@ -36,11 +36,11 @@ export default ({ data }) => {
       <Layout>
         <div className="container--homepage">
           <HomepageHeader />
+          <HomepageTileNav />
+          <HomepageWhyDoIt />
           <main className="page-content ibm--grid" id="maincontent">
             <MDXRenderer>{post.code.body}</MDXRenderer>
           </main>
-          <HomepageWhyDoIt />
-          <HomepageFooter />
         </div>
         <WebsiteBackToTopBtn />
       </Layout>
