7e2991564a15c0193829aee67a3eb8685f8b1f5d
Scott <sstrubberg@us.ibm.com>
2019-05-01 15:13:39 -0500

    navigation pages ready

--- src/components/NavTile/NavTile.js
+++ src/components/NavTile/NavTile.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ArrowRight20 } from '@carbon/icons-react';
 
 export default class NavTile extends React.Component {
   static PropTypes = {
+    children: PropTypes.string,
     href: PropTypes.string,
     title: PropTypes.string,
     description: PropTypes.string,
@@ -10,15 +12,17 @@ export default class NavTile extends React.Component {
   };
 
   render() {
-    const { href, title, description, svgicon } = this.props;
+    const { href, title, description, children } = this.props;
     return (
       <div className="ibm--col-sm-4 ibm--col-md-3 ibm--col-lg-5 nav--tile">
         <a href={href}>
           <div className="nav--tile-content-wrapper">
             <p className="bx--type-productive-heading-05">{title}</p>
             <p>{description}</p>
-            {svgicon}
-            <div>Read more</div>
+            <div>{children}</div>
+            <div className="nav--tile-readmore">
+              Read more <ArrowRight20 aria-label="read more" />
+            </div>
           </div>
         </a>
       </div>
